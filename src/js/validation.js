80e1aac5e890c1ab593edefea50874e5edea73e5
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-15 13:02:46 +0900

    feat: 처음입력이 연산자 들어올 경우 벨리데이션 추가

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,8 +1,8 @@
-import { MAX_LENGTH } from './constants.js';
 import { MAX_LENGTH,OPERATOR } from './constants.js';
 
 export const INVALID_LENGTH = `숫자는 ${MAX_LENGTH}자리까지만 입력 가능합니다!`;
 export const NOT_ENTER_NUMBER = `숫자를 먼저 입력해주세요.`;
+export const NOT_OVERLAP_NUMBER = `연산자는 중복입력이 불가능합니다.`;
 
 export const isOverMaxNumber = (operators) => {
   if (operators.length > MAX_LENGTH - 1) {
@@ -22,3 +22,12 @@ export const isOverlapOperator = (num2,operator,clikckOperator) => {
   return false;
 };
 
+export const isEnterFirstInputOperator = (num1) => {
+  if (!num1) {
+    alert(NOT_ENTER_NUMBER);
+    return true;
+  }
+
+  return false;
+
+};
